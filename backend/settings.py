905f736152265c369787336cc67e83183db36a7c
DireWolf707 <rahulsood707@gmail.com>
2021-06-14 14:36:04 +0530

    allowed cors for all

--- backend/settings.py
+++ backend/settings.py
@@ -132,6 +132,4 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ORIGINS = [
-    "https://ancient-scrubland-64264.herokuapp.com",
-]
+CORS_ALLOW_ALL_ORIGINS = True
